58baf8fbdef724d9969db1a65f90041083374bec
kushi8811 <gjorgjokushi1998@icloud.com>
2025-04-23 16:20:42 +0200

    google tag mennager added

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Lora } from "next/font/google";
 import "./globals.css";
 import Header from "./_components/Header";
 import Footer from "./_components/Footer";
+import Script from "next/script"; // ✅ Use this!
 
 const lora = Lora({
   variable: "--font-geist-sans",
@@ -11,14 +12,38 @@ const lora = Lora({
 export const metadata = {
   title: {
     template: "%s : Il Vespro",
-    default: "Il Vepsro",
+    default: "Il Vespro",
   },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${lora.className}`}>
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PC4JRT87');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
+      </head>
       <body className="bg-accent-200">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PC4JRT87"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <Header />
         {children}
         <Footer />
