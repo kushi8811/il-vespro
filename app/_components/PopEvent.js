3d0d6af231c63ec6e56187ee9f3c6995ff1080ec
Gjorgj Kushi <robertmari@MacBook-Air-di-Robert.local>
2025-09-29 13:50:45 +0100

    commit

--- app/_components/PopEvent.js
+++ app/_components/PopEvent.js
@@ -2,7 +2,7 @@
 import { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import image from "@/public/images/event.jpg";
+import image from "@/public/images/event1.jpg";
 export default function EventPopup() {
   const [show, setShow] = useState(false);
   const [animate, setAnimate] = useState(false);
